b1e92430a970bfee3d82548535040c7adfb8552f
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-02-08 15:58:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,7 +154,7 @@ def send_search_results(update: Update, context: CallbackContext, jiosaavn_resul
           track_name = item['title']
           artist_name = item['more_info']['singers']
           button_text = f"{counter}. (JioSaavn) {track_name} - {artist_name}"
-          keyboard.append([InlineKeyboardButton(button_text, callback_data=f"jiosaavn_{counter-1}"))
+          keyboard.append([InlineKeyboardButton(button_text, callback_data=f"jiosaavn_{counter-1}")])
           counter += 1
 
     if spotify_results:
@@ -162,8 +162,8 @@ def send_search_results(update: Update, context: CallbackContext, jiosaavn_resul
         track_name = item['name']
         artist_name = item['artists'][0]['name']
         button_text = f"{counter}. (Spotify) {track_name} - {artist_name}"
-        keyboard.append([InlineKeyboardButton(button_text, callback_data=f"spotify_{counter-1}"))
-        counter += 1
+        keyboard.append([InlineKeyboardButton(button_text, callback_data=f"spotify_{counter-1}")])
+        counter += 1a
 
     if not keyboard:
         update.message.reply_text("No results found.")
